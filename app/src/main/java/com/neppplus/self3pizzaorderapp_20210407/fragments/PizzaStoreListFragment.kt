0212f7aa0ffd08d4d78c0ab96449403067c2aed5
전은형 <ooooo5h91@gmail.com>
2021-04-07 23:33:58 +0900

    이게 맞지! 여기까지가 피자가게 어댑터 적용!! 그다음에는 이제 겟뷰내부수정해줘야함

--- app/src/main/java/com/neppplus/self3pizzaorderapp_20210407/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/self3pizzaorderapp_20210407/fragments/PizzaStoreListFragment.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.neppplus.self3pizzaorderapp_20210407.R
+import com.neppplus.self3pizzaorderapp_20210407.adapters.StoreListAdapter
 import com.neppplus.self3pizzaorderapp_20210407.datas.Store
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
 
@@ -15,6 +18,8 @@ class PizzaStoreListFragment : Fragment() {
 //    lateinit var mStoreList : ArrayList<Store>
 
     val mStoreList = ArrayList<Store>()
+
+    lateinit var mStoreListAdapter : StoreListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +37,12 @@ class PizzaStoreListFragment : Fragment() {
         mStoreList.add(Store("도미노피자", "",""))
         mStoreList.add(Store("미스터피자", "",""))
 
-
+        mStoreListAdapter = StoreListAdapter(activity!!, R.layout.store_list_item, mStoreList)
+//        한 줄 더있는거 같은데
+//        mainViewPager.adapter = mStoreListAdapter
+//        또 틀렸지. 뷰페이저의 어댑터는 이미 연결해줬잖아
+//        이제는 리스트뷰의 어댑터를 만들어주는 상황이니까
+        storeListView.adapter = mStoreListAdapter
 
 
     }
